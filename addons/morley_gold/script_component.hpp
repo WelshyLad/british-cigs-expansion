902321bcdbf5feb10f4c8ffbc8b3664695e3084d
Welshy <welshyplaysgames@gmail.com>
2025-09-09 18:15:43 +0100

    Added Malbro Gold

---
+++ addons/morley_gold/script_component.hpp
@@ -0,0 +1,10 @@
+#define COMPONENT morley_gold
+
+// This is being used for the Addon's Name and can be "My Addon Template Framework"
+#define COMPONENT_BEAUTIFIED Morley Gold Cigarettes
+
+
+#include "\z\cigs\addons\main\script_mod.hpp"
+#include "\z\cigs\addons\main\script_macros.hpp"
+
+#include "defines.hpp"
