de49a5062bb007e4665c23de2d8222ce6facf0a5
Welshy <welshyplaysgames@gmail.com>
2025-09-09 23:05:19 +0100

    More Cigs added

---
+++ addons/richmont/script_component.hpp
@@ -0,0 +1,10 @@
+#define COMPONENT richmont
+
+// This is being used for the Addon's Name and can be "My Addon Template Framework"
+#define COMPONENT_BEAUTIFIED Richmont Cigarettes
+
+
+#include "\z\cigs\addons\main\script_mod.hpp"
+#include "\z\cigs\addons\main\script_macros.hpp"
+
+#include "defines.hpp"
